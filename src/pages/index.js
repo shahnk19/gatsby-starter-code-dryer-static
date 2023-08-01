110615597cf09a31a8648917012feb354957521b
shahnk19 <shahnk19@gmail.com>
2023-08-01 15:23:17 +0800

    Add tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import * as React from "react"
+import * as React from "react";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <h1 className="text-3xl font-bold">Hello world!</h1>;
 }
