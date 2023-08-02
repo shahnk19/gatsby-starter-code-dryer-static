358bf678c6ec88edb0c0e994d8f57bace813b4f3
shahnk19 <shahnk19@gmail.com>
2023-08-02 14:08:50 +0800

    Add deploy to AWS S3

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,13 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "your-bucket-name",
+        region: "your-bucket-region",
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
